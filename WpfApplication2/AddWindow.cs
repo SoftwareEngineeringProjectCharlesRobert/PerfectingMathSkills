33461aca4980db5f0d6b2ab249ffa3d0397c6465
chaec <chaec@DESKTOP-P8649JM>
2016-10-05 11:48:55 -0500

    Add project files.

---
+++ WpfApplication2/AddWindow.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+
+namespace WpfApplication2
+{
+    public class AddWindow : Window
+    {
+        public AddWindow()
+        {
+            Window addition = new Window ();
+            addition.Height = 600;
+            addition.Width = 600;
+            Grid addWindowGrid = new Grid { };
+            addition.Background = Brushes.SteelBlue;
+            TextBox TopNum = new TextBox { };
+            TopNum.Height = 120;
+            TopNum.Width = 60;
+            addWindowGrid.Children.Add(TopNum);
+            addition.Content = addWindowGrid;
+            addition.Show();
+
+        }
+    }
+}
