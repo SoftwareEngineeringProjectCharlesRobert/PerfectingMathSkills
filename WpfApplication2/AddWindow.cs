7aebae25e4ed17af460c2aded664f42812c3c1ae
Charles Clayton <chaeclay@ut.utm.edu>
2016-10-12 11:45:01 -0500

    Added AnswerBox and Symbol. Changed top and bottom num to textblocks.

--- WpfApplication2/AddWindow.cs
+++ WpfApplication2/AddWindow.cs
@@ -26,17 +26,45 @@ namespace WpfApplication2
             Grid addWindowGrid = new Grid { };
             addition.Background = Brushes.SteelBlue;
 
-            TextBox TopNum = new TextBox { };
-            TopNum.Margin = new Thickness(400, 0, 0, 400);
+            TextBlock TopNum = new TextBlock { };
+            TopNum.Margin = new Thickness(400, 0, 150, 400);
+            //TopNum.Background = Brushes.White;
+            TopNum.FontSize = 100;
+            TopNum.FontFamily = new FontFamily("Cooper Black");
+            TopNum.TextAlignment = TextAlignment.Right;
+            TopNum.Text += "33333";
             TopNum.Height = 120;
-            TopNum.Width = 60;
+            TopNum.Width = 300;
+
+            TextBlock BottomNum = new TextBlock { };
+            BottomNum.Margin = new Thickness(400, 0, 150, 50);
+            //BottomNum.Background = Brushes.White;
+            BottomNum.FontSize = 100;
+            BottomNum.FontFamily = new FontFamily("Cooper Black");
+            BottomNum.TextAlignment = TextAlignment.Right;
+            BottomNum.Text += "33333";
+            BottomNum.Height = 120;
+            BottomNum.Width = 300;
+
+            TextBlock Symbol = new TextBlock ();
+            Symbol.Background = Brushes.White;
+            Symbol.Height = 60;
+            Symbol.Width = 60;
+            Symbol.Margin = new Thickness(400, 0, 700, 500);
+            Symbol.Text += "+";
+
 
             TextBox AnswerBox = new TextBox();
-            AnswerBox.Margin = new Thickness(400, 400, 0, 0);
+            AnswerBox.Margin = new Thickness(400, 400, 150, 0);
+            AnswerBox.FontFamily = new FontFamily("Cooper Black");
             AnswerBox.Height = 120;
-            AnswerBox.Width = 60;
+            AnswerBox.Width = 300;
+            AnswerBox.FontSize = 100;
+           
 
+            addWindowGrid.Children.Add(BottomNum);
             addWindowGrid.Children.Add(TopNum);
+            addWindowGrid.Children.Add(Symbol);
             addWindowGrid.Children.Add(AnswerBox);
             addition.Content = addWindowGrid;
             addition.Show();
