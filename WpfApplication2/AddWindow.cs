91a8bd759e8f2d1558ce57e4710322fd40716510
Charles Clayton <chaeclay@ut.utm.edu>
2016-10-19 11:24:53 -0500

    Added random num generator. Nums the same.

--- WpfApplication2/AddWindow.cs
+++ WpfApplication2/AddWindow.cs
@@ -17,6 +17,22 @@ namespace WpfApplication2
 {
     public class AddWindow : Window
     {
+
+        public int getTopNum(int max)
+        {               //max will be one more than difficulty bounds
+            Random randomNum = new Random();
+            int number = randomNum.Next(max);
+            return number;
+        }
+
+        public int getBotNum(int max)
+        {               //max will be one more than difficulty bounds
+            Random randomNum = new Random();
+            max = max - 1;
+            int number = randomNum.Next(max);
+            return number;
+        }
+
         public AddWindow()
         {
             Window addition = new Window();
@@ -32,7 +48,7 @@ namespace WpfApplication2
             TopNum.FontSize = 100;
             TopNum.FontFamily = new FontFamily("Cooper Black");
             TopNum.TextAlignment = TextAlignment.Right;
-            TopNum.Text += "33333";
+            TopNum.Text += getTopNum(26);
             TopNum.Height = 120;
             TopNum.Width = 300;
 
@@ -42,15 +58,17 @@ namespace WpfApplication2
             BottomNum.FontSize = 100;
             BottomNum.FontFamily = new FontFamily("Cooper Black");
             BottomNum.TextAlignment = TextAlignment.Right;
-            BottomNum.Text += "33333";
+            BottomNum.Text += getBotNum(26);
             BottomNum.Height = 120;
             BottomNum.Width = 300;
 
             TextBlock Symbol = new TextBlock ();
-            Symbol.Background = Brushes.White;
-            Symbol.Height = 60;
+            Symbol.Background = Brushes.SteelBlue;
+            Symbol.Height = 120;
             Symbol.Width = 60;
-            Symbol.Margin = new Thickness(400, 0, 700, 500);
+            Symbol.FontSize = 100;
+            Symbol.FontFamily = new FontFamily("Cooper Black");
+            Symbol.Margin = new Thickness(400, 0, 700, 50);
             Symbol.Text += "+";
 
 
